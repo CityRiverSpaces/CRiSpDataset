6ab9f65842adbd05439d1cca29bc89331078dbb5
Francesco Nattino <f.nattino@esciencecenter.nl>
2025-06-10 00:44:24 +0200

    add script to compute metrics

--- scripts/compute_metrics.R
+++ scripts/compute_metrics.R
@@ -7,29 +7,59 @@ library(sf)
 CITY_RIVERS_FILEPATH <- file.path("output", "city_rivers.csv")
 SEGMENT_DIR <- file.path("output", "segments")
 FEATURES_DIR <- file.path("output", "features")
-OUTPUT_METRICS_FILEPATH <- file.path("output", "city_rivers_metrics.csv")
+OUTPUT_METRICS_FILEPATH <- file.path("output", "city_rivers_metrics.gpkg")
 
-
+#' Main access point of the script
+#'
+#' Compute all the metrics for the (pre-computed) segments of the city rivers.
+#'
+#' @param city_rivers_filepath string
+#' @param segments_dir string
+#' @param features_dir string
+#' @param output_metrics_filepath string
 run <- function(city_rivers_filepath, segments_dir, features_dir,
                 output_metrics_filepath) {
   # Load city river table as a data frame
   city_rivers <- read.csv(city_rivers_filepath)
 
-  # Loop over the cities and retrieve the features for the available segments
+  # Loop over the cities, retrieve the segments and features for the available
+  # city rivers. For each segment, compute a list of metrics.
+  all_metrics <- NULL
   for (n in seq_len(nrow(city_rivers))) {
     cr <- city_rivers[n, ]
     city_name <- cr$city_name
     river_name <- cr$river_name
     segments <- load_vector_data(city_name, river_name, dir = segments_dir)
     if (is.null(segments)) next
+    print(paste("Computing metrics for ", city_name))
     features <- get_features(city_name, river_name, segments,
                              dir = features_dir)
-    metrics <- compute_metrics(features)
+    metrics <- compute_metrics(
+      streets = features[["streets"]],
+      railways = features[["railways"]],
+      buildings = features[["buildings"]],
+      segments = features[["segments"]],
+      blocks = features[["blocks"]],
+      sanctuary_polygons = features[["sanctuary_polygons"]]
+    )
+    metrics["city_name"] <- city_name
+    metrics["river_name"] <- river_name
+    all_metrics <- bind_rows(all_metrics, metrics)
   }
 
-  # write.csv(metrics, output_metrics_filepath)
+  # Write out metrics
+  st_write(all_metrics, output_metrics_filepath, quiet = TRUE, append = FALSE)
 }
 
+#' Get all features for the given city river
+#'
+#' Load simple features directly retrieved from OSM or precomputed via rcrisp,
+#' and compute derived features. TODO: missing tessellation cells
+#'
+#' @param city_name string
+#' @param river_name string
+#' @param segments [`sf::sfc`] object
+#' @param dir string
 get_features <- function(city_name, river_name, segments, dir = ".") {
   # OSM features
   streets <- load_vector_data(city_name, river_name, suffix = "streets", dir = dir)
@@ -41,6 +71,7 @@ get_features <- function(city_name, river_name, segments, dir = ".") {
   # Derived features
   corridor <- st_union(segments)
   composite <- get_composite(buildings)
+  blocks <- get_blocks(corridor, streets, railways, river_centerline)
   sanctuary_polygons <- get_sanctuary_polygons(corridor, streets)
 
   # Return all features
@@ -53,11 +84,43 @@ get_features <- function(city_name, river_name, segments, dir = ".") {
     river_surface = river_surface,
     # Derived OSM features
     composite = composite,
+    blocks = blocks,
     sanctuary_polygons = sanctuary_polygons,
     # CRiSp features
     corridor = corridor,
     segments = segments
   )
+}
+
+compute_metrics <- function(streets, railways, buildings, segments, blocks, sanctuary_polygons) {
+  # Set segment geometries in lat/lon, so that we can merge results for all
+  # city rivers
+  metrics <- st_transform(segments, "EPSG:4326")
+  # Metrics specific to urban river spaces
+  # Perimeter area ratio
+  metrics["cs_par"] <- get_perimeter_area_ratio(segments)
+  # Open space ratio
+  metrics["cs_osr"] <- get_open_space_ratio(segments, buildings)
+  # General urban form metrics
+  # Length of street network
+  metrics["sn_l"] <- get_network_length(segments, streets)
+  # Length of rail network
+  metrics["rn_l"] <- get_network_length(segments, railways)
+  # Density of street network
+  metrics["sn_d"] <- get_density(segments, metrics[["sn_l"]])
+  # Density of rail network
+  metrics["rn_d"] <- get_density(segments, metrics[["rn_l"]])
+  # Number of blocks
+  metrics["b_c"] <- get_number_of_blocks(segments, blocks)
+  # Density of blocks
+  metrics["b_d"] <- get_density(segments, metrics[["b_c"]])
+  # Mean sanctuary area
+  metrics["sa_a_m"] <- get_mean_sanctuary_area(segments, sanctuary_polygons)
+  # Standard deviation of sanctuary area
+  metrics["sa_a_sd"] <- get_sd_sanctuary_area(segments, sanctuary_polygons)
+
+  # Return computed metrics
+  metrics
 }
 
 load_vector_data <- function(city_name, river_name, suffix = NULL, dir = ".",
@@ -82,22 +145,87 @@ get_filepath <- function(city_name, river_name, suffix = NULL, dir = ".", ext = 
   file.path(dir, filename)
 }
 
+#' Merged OSM buildings
 get_composite <- function(buildings) {
   buildings |>
     st_union() |>
     st_cast("POLYGON")
 }
 
-get_sanctuary_polygons <- function(corridor, streets) {
-  main_streets <- streets |>
-    filter(type %in% c("motorway", "trunk", "primary", "secondary", "tertiary"))
-  st_split(corridor, main_streets) |>
+#' Approximate OSM blocks
+#'
+#' Area enclosed by all streets, railways and the river centerline.
+get_blocks <- function(corridor, streets, railways, river_centerline) {
+  lines <- c(
+    st_geometry(streets), st_geometry(railways), st_geometry(river_centerline)
+  )
+  st_split(corridor, lines) |>
     st_collection_extract()
 }
 
-#' Calculate metrics for available cities
-compute_metrics <- function(features) {
-  print("Done")
+#' Sanctuary area
+#'
+#' Derived from major OSM streets, railways and rivers according to Dibble et
+#' al. (2019). We use the default street network elements used in rcrisp
+#' ("motorway", "trunk", "primary", "secondary", "tertiary").
+get_sanctuary_polygons <- function(corridor, streets) {
+  lines <- streets |>
+    filter(type %in% c("motorway", "trunk", "primary", "secondary", "tertiary")) |>
+    st_geometry()
+  st_split(corridor, lines) |>
+    st_collection_extract()
+}
+
+
+get_perimeter_area_ratio <- function(segments) {
+  st_length(st_boundary(segments)) / st_area(segments)
+}
+
+#' Estimate Open Space Ratio (OSR) as 1. - area_buildings / area_tot
+get_open_space_ratio <- function(segments, buildings) {
+  # Calculate the OSR in the nsegment-th segment
+  calc_open_space_ratio <- function(nsegment) {
+    area_buildings <- st_intersection(st_geometry(buildings), segments[nsegment, ]) |>
+      st_as_sf() |>
+      filter(st_geometry_type(x) %in% c("POLYGON", "MULTIPOLYGON")) |>
+      st_area() |>
+      sum()
+    area_segment <- st_area(segments[nsegment, ])
+    (area_segment - area_buildings) / area_segment
+  }
+  sapply(seq_len(nrow(segments)), calc_open_space_ratio)
+}
+
+
+get_network_length <- function(segments, edges) {
+  # Calculate the total network length in the nsegment-th segment
+  calc_network_length <- function(nsegment) {
+    st_intersection(st_geometry(edges), segments[nsegment, ]) |>
+      st_as_sf() |>
+      filter(st_geometry_type(x) %in% c("LINESTRING", "MULTILINESTRING")) |>
+      st_length() |>
+      sum()
+  }
+  sapply(seq_len(nrow(segments)), calc_network_length)
+}
+
+get_density <- function(segments, x) {
+  x / st_area(segments)
+}
+
+get_number_of_blocks <- function(segments, blocks) {
+  intersections <- st_intersects(segments, blocks)
+  sapply(intersections, length)
+}
+
+get_mean_sanctuary_area <- function(segments, sanctuary_polygons) {
+  intersections <- st_intersects(segments, sanctuary_polygons)
+  sapply(intersections, \(n) mean(st_area(sanctuary_polygons[n])))
+}
+
+get_sd_sanctuary_area <- function(segments, sanctuary_polygons) {
+  intersections <- st_intersects(segments, sanctuary_polygons)
+  sapply(intersections, \(n) sd(st_area(sanctuary_polygons[n])))
 }
 
 # Call the main function
